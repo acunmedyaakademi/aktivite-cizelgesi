81693be19ef3a0e0c79521b474514ef4b9742d77
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2024-12-08 21:49:56 +0300

    succesfuly updated

---
+++ aktivite-cizelgesi.js
@@ -0,0 +1,23 @@
+// Bir haftalık aktivite çizelgesi oluşturun. Kullanıcıdan her gün için bir gün adı ve o güne ait bir aktivite girilmesini isteyin. Girilen bilgileri bir diziye obje olarak kaydedin ve tüm çizelgeyi konsola yazdırın
+
+
+let haftalikCizelge = [
+    {
+
+    }
+];
+for (let i = 1; i <= 7; i++) {
+    gun = prompt(`${i}. gün için günün adi`);
+    aktivite = prompt(`${i}. gün için aktivite giriniz.`);
+
+    cizelge = {
+        gun: gun,
+        aktivite: aktivite,
+    }
+    haftalikCizelge.push(cizelge);
+}
+console.log(haftalikCizelge);
+
+
+
+
